73de522a3bdb6d4dc5057629d4255d0cf18215bb
Thomas Maulon <tmaulon@betomorrow.com>
2019-05-03 13:53:59 +0200

    Modifs : join-us.js + new _error.js page

--- pages/join-us.js
+++ pages/join-us.js
@@ -3,6 +3,41 @@ import React, { Component, Fragment } from "react";
 import axios from "axios";
 import Link from "next/link";
 
+const offerListStye = {
+  display: "flex",
+  justifyContent: "flex-start",
+  flexWrap: "wrap",
+  height: "100%",
+  maxWidth: "1200px",
+  width: "100%",
+  margin: "0 auto",
+  padding: "30px",
+  borderRadius: "15px"
+};
+const offerCardWrapperStyle = {
+  display: "block",
+  borderRadius: "6px",
+  minHeight: "200px",
+  height: "auto",
+  flex: "0 0 100%",
+  maxWidth: "32%",
+  margin: "0 4px 45px",
+  textAlign: "center",
+  marginBottom: "35px"
+};
+const offerCardStyle = {
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  flexFlow: "row wrap",
+  height: "100%",
+  position: "relative",
+  bottom: 0,
+  backgroundColor: "#fff",
+  boxShadow: "0 10px 20px 0px rgba(0, 0, 0, 0.1)",
+  transition: "all .3s ease-in-out"
+};
+
 export default class extends Component {
   //Resolve promise and set initial props.
   static async getInitialProps() {
@@ -21,8 +56,8 @@ export default class extends Component {
     return (
       <Fragment>
         <Navigation />
-        <h1>Nos Posts !</h1>
-        <ul>
+        <h1>Nos offres</h1>
+        <ul style={offerListStye}>
           {this.props.offers.map(offer => (
             <li
               key={offer.id}
@@ -30,11 +65,13 @@ export default class extends Component {
                 "bto_join_us_offer_card_wrapper bto_join_us_offer_card_wrapper__" +
                 offer.id
               }
+              style={offerCardWrapperStyle}
             >
               <Link href={`/nous-rejoindre/${offer.slug}`}>
                 <a
                   href={`/nous-rejoindre/${offer.slug}`}
                   title={`${offer.title.rendered}`}
+                  style={offerCardStyle}
                 >
                   {offer.title.rendered}
                 </a>
